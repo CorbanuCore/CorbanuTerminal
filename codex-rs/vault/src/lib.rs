71afdd42625723ce8ad3dc0d6c0b4d1e4f7f4023
IridiumMaster <travis.godwin.good@gmail.com>
2026-09-04 18:05:43 -0700

    Wire isolated broker Core Vault and proxy integration seams

--- codex-rs/vault/src/lib.rs
+++ codex-rs/vault/src/lib.rs
@@ -57,6 +57,11 @@ mod credential_panic;
 
 pub use capability::ScopedCredentialCallbackError;
 pub use capability::ScopedCredentialError;
+pub use capability::SystemVaultBrokerClock;
+pub use capability::VaultBrokerBackend;
+pub use capability::VaultBrokerBackendError;
+pub use capability::VaultBrokerClock;
+pub use capability::VaultBrokerTransport;
 pub use capability::VaultCredentialRef;
 pub use claude_auth::CREDENTIALS_FILE_CLAUDE_AUTH_SOURCE_ID;
 pub use claude_auth::ClaudeAuthHealth;
